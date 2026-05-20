2a04eb4e5ddf4f4304d0914c663b0ea345d84bb3
nwlterry <nwl.terry@gmail.com>
2026-05-20 15:51:16 +0800

    Update Deploy script to target SSRS Internal Web Server certs and new main script name

--- Deploy-CertificateRenewalTasks.ps1
+++ Deploy-CertificateRenewalTasks.ps1
@@ -1,35 +1,37 @@
-# Based on https://social.technet.microsoft.com/wiki/contents/articles/14250.certificate-services-lifecycle-notifications.aspx
-# Look for any certificates with relevant 'Certificate Template Information', and deploy a new task to be triggered when the certificate is replaced
-# If the script deployed is updated, recreate the task - will also re-trigger the task to update any existing certificates
+# Deploy-CertificateRenewalTasks.ps1
+# Based on: https://social.technet.microsoft.com/wiki/contents/articles/14250.certificate-services-lifecycle-notifications.aspx
 
-$CertificateNotificationTaskName = "Internal-SystemCertificateRenewalTask"
+$CertificateNotificationTaskName = "SSRS-SystemCertificateRenewalTask"
 $NotificationScriptFile = "Update-RenewedSystemCertificates.ps1"
 $ScriptDestinationPath = "$env:SystemDrive\#PowerShell"
 
-$NPSServerCertificates = Get-ChildItem Cert:\LocalMachine\My\ | Where-Object { $_.Extensions | Where-Object { $_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=RAS and IAS Server\(" } }
-$SQLServerCertificates = Get-ChildItem Cert:\LocalMachine\My\ | Where-Object { $_.Extensions | Where-Object { $_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=SQL Server\(" } }
-$WinRMCertificates = Get-ChildItem Cert:\LocalMachine\My\ | Where-Object { $_.Extensions | Where-Object { $_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=WinRM\(" } }
-$WMSvcCertificates = Get-ChildItem Cert:\LocalMachine\My\ | Where-Object { $_.Extensions | Where-Object { $_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=WMSvc\(" } }
-$HyperVCertificates = Get-ChildItem Cert:\LocalMachine\My\ | Where-Object { $_.Extensions | Where-Object { $_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=Hyper-V\(" } }
-$HyperVReplicaCertificates = Get-ChildItem Cert:\LocalMachine\My\ | Where-Object { $_.Extensions | Where-Object { $_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=Hyper-V Replica\(" } }
-$NDESServerCertificates = Get-ChildItem Cert:\LocalMachine\My\ | Where-Object { $_.Extensions | Where-Object { ($_.Oid.Value -eq '1.3.6.1.4.1.311.20.2' -and $_.Format(0) -eq "CEPEncryption") -or `
-    ($_.Oid.Value -eq '1.3.6.1.4.1.311.20.2' -and $_.Format(0) -eq "EnrollmentAgentOffline") -or `
-    ($_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=NDES CEP Encryption\(") -or `
-    ($_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=NDES Exchange Enrollment Agent \(Offline Request\)\(") } }
+# Target Internal Web Server certs for SSRS (add others if needed)
+$InternalWebCerts = Get-ChildItem Cert:\LocalMachine\My | Where-Object {
+    $_.Extensions | Where-Object { $_.Oid.Value -eq '1.3.6.1.4.1.311.21.7' -and $_.Format(0) -match "^Template=Internal Web Server\(" }
+}
 
-if ($null -ne $NPSServerCertificates -or $null -ne $SQLServerCertificates -or $null -ne $WinRMCertificates -or $null -ne $WMSvcCertificates -or $null -ne $HyperVCertificates -or $null -ne $HyperVReplicaCertificates -or $null -ne $NDESServerCertificates) {
-    if (-not (Test-Path -Path $ScriptDestinationPath -PathType Container)) { # Create the destination directory if it does not already exist
-        New-Item $ScriptDestinationPath -Type Directory | Out-Null
+if ($InternalWebCerts) {
+    if (-not (Test-Path $ScriptDestinationPath -PathType Container)) {
+        New-Item $ScriptDestinationPath -ItemType Directory -Force | Out-Null
     }
 
-    # If the destination script file does not yet exist, or is not up to date
-    if (-not (Test-Path -Path "$ScriptDestinationPath\$NotificationScriptFile" -PathType Leaf) -or (Get-FileHash -Path "$PSScriptRoot\$NotificationScriptFile").Hash -ne (Get-FileHash -Path "$ScriptDestinationPath\$NotificationScriptFile").Hash) {
-        Write-Output "Deploying certificate notification script '$ScriptDestinationPath\$NotificationScriptFile'..."
-        Copy-Item -Path "$PSScriptRoot\$NotificationScriptFile" -Destination $ScriptDestinationPath -Force
-        Unblock-File -Path "$ScriptDestinationPath\$NotificationScriptFile" # Unblock the file if it is still marked as downloaded from the Internet
+    $SourceScript = Join-Path $PSScriptRoot $NotificationScriptFile
+    $DestScript = Join-Path $ScriptDestinationPath $NotificationScriptFile
 
-        if (Get-CertificateNotificationTask | Where-Object { $_.Name -eq $CertificateNotificationTaskName }) { Remove-CertificateNotificationTask -Name $CertificateNotificationTaskName } # If the named certificate notification task already exists, first remove it
+    if (-not (Test-Path $DestScript) -or (Get-FileHash $SourceScript).Hash -ne (Get-FileHash $DestScript).Hash) {
+        Write-Output "Deploying/updating notification script..."
+        Copy-Item $SourceScript $DestScript -Force
+        Unblock-File $DestScript
+
+        if (Get-CertificateNotificationTask | Where-Object { $_.Name -eq $CertificateNotificationTaskName }) {
+            Remove-CertificateNotificationTask -Name $CertificateNotificationTaskName
+        }
+
         Write-Output "Deploying certificate notification task '$CertificateNotificationTaskName'..."
-        New-CertificateNotificationTask -Type Replace -RunTaskForExistingCertificates -PSScript "$ScriptDestinationPath\$NotificationScriptFile" -Name $CertificateNotificationTaskName -Channel System | Out-Null
+        New-CertificateNotificationTask -Type Replace `
+            -RunTaskForExistingCertificates `
+            -PSScript $DestScript `
+            -Name $CertificateNotificationTaskName `
+            -Channel System | Out-Null
     }
 }
